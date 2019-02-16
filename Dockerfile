eb1e19cb550a1d5a79d47430e35ccb217ab5fcb0
Carl Njoku <carlnjoku@Carls-MacBook-Pro.local>
2019-02-16 13:31:39 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:latest
+MAINTAINER Carl Njoku "flavoursoft@yahoo.com"
+RUN apt-get update -y
+RUN apt-get install -y python-pip python-dev build-essential
+COPY . /app
+WORKDIR /app
+RUN pip install -r requirements.txt
+ENTRYPOINT ["python"]
+CMD ["image_upload.py"]
+
+
---
+++ image_upload.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+import shutil
+import os
+
+#import nude
+#from nude import Nude
+from nudity import Nudity
+
+def is_nude(fileName):
+
+    nudity = Nudity();
+    nude_ret = nudity.has(fileName)
+    # gives you True or False
+
+    #print(nudity.score(fileName))
+
+    return nude_ret
+    # gives you nudity score 0 - 1
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads/images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    res = {}
+
+    if 'image' in request.files and 'memberID' in request.form :
+        file = request.files['image']
+        filename = request.form['memberID'] + ".jpg"
+    else:
+        if not 'image' in request.files :res["error"] = "No Image"
+        if not 'memberID' in request.form :res["error"] = "No user ID provided"
+        return jsonify({"data": res})
+
+    filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
+    temp_file = os.path.join(app.config['UPLOAD_FOLDER'], "temp.jpg")
+    file.save(temp_file)
+
+    # check nudity validation
+    if is_nude(temp_file):
+        res["msg"] = "Nude_Image"
+    else:
+        res["msg"] = "Valid_Image"
+        shutil.copy(temp_file,filename)
+
+    os.remove(temp_file)
+
+    return jsonify({"data": res})
+
+if __name__ == '__main__':
+    app.run(port='5001', debug=True)
